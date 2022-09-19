f17143024a4fc40079681bc5f26337c1102652e3
Alina Lavrik <alina_voro@mail.ru>
2022-09-19 22:13:47 +0300

    добавили условие равенства

--- TASK_01/Program.cs
+++ TASK_01/Program.cs
@@ -1,21 +1,24 @@
-﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿/* Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 a = 5; b = 7 -> max = 7
 a = -9 b = -3 -> max = -3*/
 
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите второе число: ");
 int b = int.Parse(Console.ReadLine());
 
 if (a > b) 
 {
-    Console.WriteLine($"max = {a}, min = {b}.");
+    Console.WriteLine($"max = {a}, min = {b}");
 }
 
 else if (b > a)
 {
-     Console.WriteLine($"max = {b}, min = {a}.");
+     Console.WriteLine($"max = {b}, min = {a}");
 }
-
+else 
+{
+    Console.WriteLine("числа равны");
+}
